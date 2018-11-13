05995d959c56a0c46d492d103ae18206cc719041
Dillorom <dabdulloeva@gmail.com>
2018-11-13 14:42:17 -0800

    create base Book and Challenge component and css

---
+++ src/components/Book/Book.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Book.css';
+
+const book = (props) => (
+    <book className="Book">
+        <h1>Title</h1>
+        <div className="Info">
+            <div classname="Author">Author</div>
+        </div>
+    </book>
+
+);
+
+export default book;
--- src/containers/Challenge/Challenge.js
+++ src/containers/Challenge/Challenge.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Book from '../../components/Book/Book';
+import FullBook from '../../components/FullBook/FullBook';
+import NewBook from '../../components/NewBook/NewBook';
+
+import './Challenge.css';
 
 class Challenge extends Component {
-    render() {
-        return(
+    render () {
+        return (
             <div>
-                <p>Test</p>
+                <section className="Books">
+                    <Book />
+                    <Book />
+                    <Book />
+                </section>
+                <section>
+                    <FullBook />
+                </section>
+                <section>
+                    <NewBook />
+                </section>
             </div>
         );
     }
-};
+}
 
 export default Challenge;
