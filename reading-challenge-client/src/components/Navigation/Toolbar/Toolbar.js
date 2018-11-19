1b2651910a1d973ab50f4b8a4deee58e1479390c
Dillorom <dabdulloeva@gmail.com>
2018-11-19 10:13:35 -0800

    move to final project folder

--- reading-challenge-client/src/components/Navigation/Toolbar/Toolbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Toolbar.css';
-
-const toolbar = (props) => (
-    <div>Toolbar</div>
-);
-
-export default toolbar;
--- reading-challenge-client/src/store/rootReducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-//need to set initial state
-}
-
-const rootReducer = (state = initialState, action) => {
-    return state
-}
-
-export default rootReducer;
