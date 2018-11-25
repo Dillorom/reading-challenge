645fd08cf7bce6119b8b6bf65dee25ee14eb8aca
Dillorom <dabdulloeva@gmail.com>
2018-11-25 15:04:59 -0800

    catch errors in loginform

--- src/containers/Challenge/LoginForm/LoginForm.js
+++ src/containers/Challenge/LoginForm/LoginForm.js
@@ -21,7 +21,10 @@ class LoginForm extends Component {
         const errors = this.validate(this.state.data);
         this.setState({errors});
         if (Object.keys(errors).length === 0){
-            this.props.submit(this.state.data);
+            this.props.submit(this.state.data)
+            .catch(err => this.setState({
+                errors: err.response.data.errors
+            }));
         }
 
     }
