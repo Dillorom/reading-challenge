fd829aae237116deef91777b753aa6ea4883e92a
Dillorom <dabdulloeva@gmail.com>
2018-11-25 21:47:55 -0800

    add loading to onsubmit and form

--- src/containers/Challenge/LoginForm/LoginForm.js
+++ src/containers/Challenge/LoginForm/LoginForm.js
@@ -21,9 +21,12 @@ class LoginForm extends Component {
         const errors = this.validate(this.state.data);
         this.setState({errors});
         if (Object.keys(errors).length === 0){
+            this.setState({
+                loading: true
+            })
             this.props.submit(this.state.data)
             .catch(err => this.setState({
-                errors: err.response.data.errors
+                errors: err.response.data.errors, loading: false
             }));
         }
 
@@ -35,9 +38,9 @@ class LoginForm extends Component {
         return errors;
       };
     render(){  
-        const { data, errors } = this.state
+        const { data, errors, loading } = this.state
         return(
-            <form onSubmit={this.onSubmit}>
+            <form onSubmit={this.onSubmit} loading={loading}>
                 <label htmlFor="Email">Email</label>
                 <p><input type="email" 
                     name="email"
