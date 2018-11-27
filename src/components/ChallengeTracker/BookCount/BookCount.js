ef4d0dc5e0985a3e209d27024bc9ac2732959898
Dillorom <dabdulloeva@gmail.com>
2018-11-27 15:24:48 -0800

    update bookCount and create booklist and bookrow, not using last two components for now

--- src/components/ChallengeTracker/BookCount/BookCount.js
+++ src/components/ChallengeTracker/BookCount/BookCount.js
@@ -2,8 +2,43 @@ import React from 'react';
 //import { PropTypes } from 'react';
 
 
-const bookCount = ({total=85, goal=100}) => {
+// const bookCount = ({total=85, goal=100}) => {
     
+//     const percentToDecimal = (decimal) => {
+//         return ((decimal * 100) +  '%')
+//     }
+    
+//     const calcGoalProgress = (total, goal) => {
+//         return percentToDecimal(total/goal)
+//     }
+//     return(
+//     <div className="book-count">
+//          <div className="toatl-days"><h1>You are  
+//             <span>  
+//                 {calcGoalProgress(
+//                     total, 
+//                     goal
+//                 )}
+//             </span> done with your challenge.</h1>
+// 		</div>
+//         <div className="">
+//             <h1>Your goal is to read {goal} books this year.</h1>
+//         </div>
+//         <h1>You have read {total} books so far.</h1>
+        
+//     </div>
+//     )
+// }
+
+// // bookCount.propTypes = {
+// //     total: PropTypes.number,
+// //     goal: PropTypes.number
+// // }
+// export default bookCount;
+
+
+const BookCount = (props) => {
+
     const percentToDecimal = (decimal) => {
         return ((decimal * 100) +  '%')
     }
@@ -12,26 +47,20 @@ const bookCount = ({total=85, goal=100}) => {
         return percentToDecimal(total/goal)
     }
     return(
-    <div>
-         <div><h1>You are  
-            <span>  
-                {calcGoalProgress(
-                    total, 
-                    goal
-                )}
-            </span> done with your challenge.</h1>
-		</div>
-        <h1>Your goal is to read {goal} books this year.</h1>
-        <h1>You have read {total} books so far.</h1>
-        
-    </div>
+        <div className="book-count">
+            <div className="total-books">
+                <h1>You have read <span>{props.total}</span> books so far.</h1>
+            </div>
+            <div className="goal">
+                <h1>Your goal is to read <span>{props.goal}</span> books this year.</h1>
+            </div>
+            <div className="progress">
+                <h1> You have made <span>{calcGoalProgress(
+                    props.total, 
+                    props.goal
+                )}</span> progress.</h1>
+            </div>
+        </div>
     )
 }
-
-// bookCount.propTypes = {
-//     total: PropTypes.number,
-//     goal: PropTypes.number
-// }
-export default bookCount;
-
-
+export default BookCount;
--- src/components/ChallengeTracker/ChallengeTracker.js
+++ src/components/ChallengeTracker/ChallengeTracker.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './ChallengeTracker.css';
+import BookList from './BookList/BookList';
 import BookCount from './BookCount/BookCount';
 
 
 const ChallengeTracker = (props) => (
     <div className="ChallengeTracker">
-       <BookCount />
+       {/* <BookList books={
+           [
+               {
+                    date: new Date("11/27/2018"),
+                    title: "Power of Meaning",
+                    author: "Emily Esfahani Smith",
+                    img_url: "https://images.gr-assets.com/books/1465653308l/30008950.jpg",
+​​                    description: "This wise, stirring book argues that the search for meaning can immeasurably deepen our lives and is far more fulfilling than the pursuit of personal happiness."
+               },
+               {
+                    date: new Date("11/26/2018"),    
+                    title: "Designing Your Life",
+                    author: " Bill Burnett, Dave Evans",
+                    img_url: "https://images.gr-assets.com/books/1476133952l/26046333.jpg",
+                    description: "Whether we’re 20, 40, 60 or older, many of us are still looking for an answer to that perennial question, ‘What do you want to be when you grow up?’ "  
+               }
+           ]
+       }/> */}
+       <BookCount total={85} goal={100}/>
     </div>
     
 );
